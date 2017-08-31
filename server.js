b9f1e8486dee1bb07fee2640715124d192994c74
abdul sarnor <teamcodebehind@hotmail.com>
2017-08-31 00:23:28 -0700

    removed unused packages

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 // app
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
-// parse application/json
-app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, '/dist')));
 
 
 app.listen( process.env.PORT || 8080, () => {
-    console.log('This app listening on port 3000!');
+    console.log('This app listening on port 8080!');
 });
 
 app.get('/*', (req, res)=> {
